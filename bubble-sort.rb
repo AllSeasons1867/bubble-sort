d8525cfc1c1d03ab3610ac8bee8d7beacbca4b9d
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-12 23:47:17 -0600

    solution implemented

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,8 +1,21 @@
-def bubbleSort(array)
-    array.length.times do |x|
-        
+def bubble_sort(arr)
+    n = arr.length
+    swapped = true
+  
+    while swapped
+      swapped = false
+  
+      (n - 1).times do |i|
+        if arr[i] > arr[i + 1]
+          arr[i], arr[i + 1] = arr[i + 1], arr[i] # Swap elements
+          swapped = true
+        end
+      end
     end
-end
+  
+    arr
+  end
+  
+  array_to_sort = [64, 25, 12, 22, 11]
 
-bubbleSort([1,4,6,1,2,3,5,412421,533])
-
+  
